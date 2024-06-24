704e6f68f26d248c1798ca935375f004994954a9
Silver-RP <163554418+Silver-RP@users.noreply.github.com>
2024-06-24 09:57:33 +0700

    Delete Desktop directory

--- Desktop/Java-Project/kenstore/src/main/java/com/bootsmytool/kenstore/services/ProductsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bootsmytool.kenstore.services;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.bootsmytool.kenstore.models.Product;
-
-public interface ProductsRepository extends JpaRepository<Product, Integer>{
-
-}
